19b1c09fa76d501327a620a571e8afe979202311
Nicholas Shindler <shindler25@gmail.com>
2018-01-11 15:35:57 -0800

    styling and stuff

--- app/index.js
+++ app/index.js
@@ -5,11 +5,13 @@ const app = {
       <div class="header-bar">
         <label>
           Enter a name to make it more personal
-          <h2> Please complete this sentence for <input type="text"/></h2>
+          <h2> Please complete this sentence for <input type="text" name="name" v-model="name" placeholder="name" :size="inputLength"/></h2>
         </label>
       </div>
       <div class="main-body-bar">
-        I like to ...
+        <div>
+          {{ fakeJson.start }} {{ fakeJson.inputs.volunteer.label }}
+        </div>
       </div>
       <div class="bottom-bar">
         <button>DONE</button>
@@ -17,6 +19,58 @@ const app = {
     </div>
   `,
   name: "app",
+  data() {
+    return {
+      name: "",
+      fakeJson: {
+        start: "I like to",
+        inputs: {
+          volunteer: {
+            label: "volunteer",
+            next: "by",
+            inputs: {
+              elderly: {
+                label: "helping the elderly",
+              },
+              kitchens: {
+                label: "going to soup kitchens",
+              },
+              running: {
+                label: "running charity races",
+              },
+              other: {
+                label: "giving my time in other ways",
+              },
+            },
+          },
+          donate: {
+            label: "donate",
+            next: "by giving",
+            inputs: {
+              books: {
+                label: "books",
+              },
+              food: {
+                label: "food",
+              },
+              money: {
+                label: "money",
+              },
+            },
+          },
+        },
+      },
+    };
+  },
+  computed: {
+    inputLength() {
+      const len = this.name.length;
+      if (len < 5) {
+        return 5;
+      }
+      return len;
+    }
+  },
 };
 
 
